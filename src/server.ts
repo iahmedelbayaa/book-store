2260f58994a052644e8a57d7c14cbf9fb25a92a8
iahmedelbayaa <elbayaaahmed845@gmail.com>
2023-12-18 20:08:49 +0200

    setup-swagger-file

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,10 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import noteRouter from './router/note-router';
 import storeRouter from './router/store-router';
-import bookRouter from './router/book-router'
+import bookRouter from './router/book-router';
+import swaggerUi from 'swagger-ui-express';
+const swaggerDocument = require('./swagger.json');
+
 var app = express();
 
 app.use(cors());
@@ -23,7 +26,7 @@ app.get('/', (req, res) => {
 app.use('/api/v1', noteRouter);
 app.use('/api/v1', storeRouter);
 app.use('/api/v1', bookRouter)
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 const port = 3000;
